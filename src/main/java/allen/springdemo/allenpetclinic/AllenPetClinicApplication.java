71d5ade26c77d0305a6e5b52cfd94c2dd7994ff9
PoChangLin <allen4353968@gmail.com>
2020-08-16 23:44:21 +0800

    First commit

---
+++ src/main/java/allen/springdemo/allenpetclinic/AllenPetClinicApplication.java
@@ -0,0 +1,13 @@
+package allen.springdemo.allenpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllenPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllenPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/allen/springdemo/allenpetclinic/AllenPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package allen.springdemo.allenpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllenPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
