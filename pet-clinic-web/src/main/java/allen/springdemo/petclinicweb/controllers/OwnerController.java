9f520b14ff4a17a868773f39bb7eff95a4b1357f
PoChangLin <allen4353968@gmail.com>
2020-08-22 16:29:41 +0800

    adding owner page and controller. close #11

--- pet-clinic-web/src/main/java/allen/springdemo/petclinicweb/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/allen/springdemo/petclinicweb/controllers/OwnerController.java
@@ -3,13 +3,13 @@ package allen.springdemo.petclinicweb.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("vets")
+@RequestMapping("owners")
 @Controller
-public class VetController {
+public class OwnerController {
 
     @RequestMapping({"","/"})
-    public String listVets(){
-        return "vets/index";
+    public String listOwners(){
+        return "owners/index";
     }
 
 }
