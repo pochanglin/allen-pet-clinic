977a9cfd8a79554dec4e9912b4ccd439961327f5
PoChangLin <allen4353968@gmail.com>
2020-08-22 16:19:28 +0800

    adding vets page and controller. closes #10

---
+++ pet-clinic-web/src/main/java/allen/springdemo/petclinicweb/controllers/VetController.java
@@ -0,0 +1,14 @@
+package allen.springdemo.petclinicweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets/index"})
+    public String listVets(){
+        return "vets/index";
+    }
+
+}
