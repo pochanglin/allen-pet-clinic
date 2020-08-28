05c79a4968efc9d8cd79e735e3c53333581e389b
PoChangLin <allen4353968@gmail.com>
2020-08-28 23:23:39 +0800

    update map impl to set id. close #21

--- pet-clinic-data/src/main/java/allen/springdemo/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/allen/springdemo/petclinic/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package allen.springdemo.petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import allen.springdemo.petclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -16,8 +15,15 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T obj){
-        map.put(id,obj);
+    T save(T obj){
+        if (obj != null){
+            if (obj.getId() == null){
+                obj.setId(getNextId());
+            }
+            map.put(obj.getId(),obj);
+        }else{
+            throw new RuntimeException("obj can not be null");
+        }
         return obj;
     }
 
@@ -27,5 +33,15 @@ public abstract class AbstractMapService<T, ID> {
 
     void deleteById(ID id){
         map.remove(id);
+    }
+
+    private Long getNextId(){
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        }catch (NoSuchElementException e){
+            nextId = 1l;
+        }
+        return nextId;
     }
 }
