f08fb45876b71a363c0f797b685f371e655bcdca
PoChangLin <allen4353968@gmail.com>
2020-09-13 15:18:16 +0800

    add pet type service. close #27.

---
+++ pet-clinic-data/src/main/java/allen/springdemo/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package allen.springdemo.petclinic.services;
+
+import allen.springdemo.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/allen/springdemo/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,34 @@
+package allen.springdemo.petclinic.services.map;
+
+import allen.springdemo.petclinic.model.PetType;
+import allen.springdemo.petclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
