fb1dcff167c33e3177c14cc0895a3a54f341913e
PoChangLin <allen4353968@gmail.com>
2020-12-21 22:16:23 +0800

    add spring data jpa service for specialty. close #42

---
+++ pet-clinic-data/src/main/java/allen/springdemo/petclinic/services/jpa/SpecialtyServiceJPA.java
@@ -0,0 +1,47 @@
+package allen.springdemo.petclinic.services.jpa;
+
+import allen.springdemo.petclinic.model.Speciality;
+import allen.springdemo.petclinic.repositories.SpecialtyRepository;
+import allen.springdemo.petclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class SpecialtyServiceJPA implements SpecialtyService {
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtyServiceJPA(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
